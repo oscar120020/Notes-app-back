9899bf0150f958e9c3271b215a8caba8efebf77e
oscar120020 <oscarmiguel12002@gmail.com>
2025-07-11 23:42:50 -0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,7 @@ async function bootstrap() {
 
   // Configurar CORS
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'http://localhost:5173',
-      'http://localhost:4200',
-    ],
+    origin: [process.env.FRONTEND_URL || 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
